0dfab417180f3a2af4934416f30372c1b898eb0b
Ishaan <ishbharal@gmail.com>
2020-12-16 08:56:46 -0600

    correct title header

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Random Google Images</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
